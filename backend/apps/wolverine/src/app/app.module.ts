e3b6b653671ac683bd9d51ff1e22e7b7bca6a382
Zachti <114566720+Zachti@users.noreply.github.com>
2024-01-01 20:23:35 +0200

    new app created - wolverine

--- backend/apps/wolverine/src/app/app.module.ts
+++ backend/apps/wolverine/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UploadModule } from '@backend/heimdall';
 import { LoggerModule } from '@backend/logger';
 import { awsConfigObject, DynamicConfigModule } from '@backend/config';
-import { heimdallConfigObject } from '@backend/heimdall';
-
 @Module({
   imports: [
-    UploadModule,
-    LoggerModule.forRoot({ serviceName: 'heimdall' }),
+    LoggerModule.forRoot({ serviceName: 'wolverine' }),
     DynamicConfigModule.forRoot({
       isGlobal: true,
-      configObjects: [heimdallConfigObject, awsConfigObject],
+      configObjects: [wolverineConfigObject, awsConfigObject],
       validationOptions: { presence: 'required' },
-    }),
-  ],
+    }),],
+  providers: [],
 })
-export class HeimdallCoreModule {}
+export class WolverineCoreModule {}
