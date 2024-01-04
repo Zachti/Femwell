9e9dfffe1af583960ac6b430f8d3f5a94293cea1
Zachti <114566720+Zachti@users.noreply.github.com>
2024-01-04 18:35:27 +0200

    heimdall service runs perfectly

--- backend/libs/heimdall/api/src/lib/config/heimdall.config.ts
+++ backend/libs/heimdall/api/src/lib/config/heimdall.config.ts
@@ -5,12 +5,14 @@ export const heimdallConfig = registerAs('heimdall', () => ({
   awsBucket: process.env['AWS_BUCKET'],
   baseFolderLocation: process.env['BUCKET_LOCATION_BASE_FOLDER'],
   maxFileSize: process.env['MAX_FILE_SIZE'],
+  s3Endpoint: process.env['AWS_S3_ENDPOINT'],
 }));
 
 const heimdallConfigurationValidationSchema = Joi.object({
   AWS_BUCKET: Joi.string().required(),
   BUCKET_LOCATION_BASE_FOLDER: Joi.string().required(),
   MAX_FILE_SIZE: Joi.string().required(),
+  AWS_S3_ENDPOINT: Joi.string().required(),
 });
 
 export const heimdallConfigObject = {
--- backend/libs/heimdall/api/src/lib/health/health-indicators.provder.ts
+++ backend/libs/heimdall/api/src/lib/health/health-indicators.provder.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { HealthIndicatorFunction } from '@nestjs/terminus';
-import { S3HealthIndicator } from './indicators/s3.indicator';
+import { Inject, Injectable } from '@nestjs/common';
+import { HealthIndicatorFunction, HttpHealthIndicator } from '@nestjs/terminus';
 import { HealthIndicatorsProvider } from '@backend/infrastructure';
+import { ConfigType } from '@nestjs/config';
+import { heimdallConfig } from '../config/heimdall.config';
 
 @Injectable()
 export class HeimdallHealthIndicatorsProvider implements HealthIndicatorsProvider {
-  constructor(private s3: S3HealthIndicator) {}
+  constructor(
+    @Inject(heimdallConfig.KEY) private readonly heimdallCfg: ConfigType<typeof heimdallConfig>,
+              private readonly http: HttpHealthIndicator) {}
 
   async getIndicators(): Promise<Array<HealthIndicatorFunction>> {
-    return [() => this.s3.isHealthy('s3')]
+    return [() => this.http.pingCheck('s3', this.heimdallCfg.s3Endpoint!)]
   }
 }
