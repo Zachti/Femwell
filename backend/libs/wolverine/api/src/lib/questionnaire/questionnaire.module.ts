552eb02ac580d53092211efc4bbfa7bf8d4f869f
Zachti <114566720+Zachti@users.noreply.github.com>
2024-01-25 15:52:08 +0200

    fixed error

--- backend/libs/wolverine/api/src/lib/questionnaire/questionnaire.module.ts
+++ backend/libs/wolverine/api/src/lib/questionnaire/questionnaire.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuestionnaireService } from './questionnaire.service';
 import { QuestionnaireResolver } from './questionnaire.resolver';
-import { QuestionnaireRepository } from './repository/Questionnaire.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  providers: [
-    QuestionnaireResolver,
-    QuestionnaireService,
-    QuestionnaireRepository,
-  ],
+  providers: [QuestionnaireResolver, QuestionnaireService],
 })
 export class QuestionnaireModule {}
