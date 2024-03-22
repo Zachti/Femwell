a10becf6302ddb668cc150ecf8d09eefd1e19326
BeBo1337 <yoleb123@gmail.com>
2024-03-22 14:04:38 +0200

    LiveChat and SideMenu cosmetics

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -13,6 +13,7 @@ import {
   VStack,
   useMediaQuery,
   TagLeftIcon,
+  Avatar,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
@@ -53,8 +54,20 @@ const SideMenu: FC<SideMenuProps> = ({
         <DrawerHeader mb="-4">
           {authUser ? (
             <>
-              <Box>Welcome Back!</Box>
-              <Box>{`${authUser.username}`}</Box>
+              <Flex direction="row" align="center">
+                <VStack align="start">
+                  <Box>Welcome Back!</Box>
+                  <Box>{`${authUser.username}`}</Box>
+                </VStack>
+                <Avatar
+                  bgColor={"pink.500"}
+                  color={"white"}
+                  name={`${authUser.username}`}
+                  size="lg"
+                  src={`${authUser.pfpURL}`}
+                  ml="auto"
+                />
+              </Flex>
             </>
           ) : (
             <>
