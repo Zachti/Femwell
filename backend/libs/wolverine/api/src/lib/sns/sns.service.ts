da7504f950a2398bc54f1d039f9a1996a8d5c210
Zachti <114566720+Zachti@users.noreply.github.com>
2024-05-11 21:39:28 +0300

    backend - added logs to sns service.

--- backend/libs/wolverine/api/src/lib/sns/sns.service.ts
+++ backend/libs/wolverine/api/src/lib/sns/sns.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { SNS } from '@aws-sdk/client-sns';
 import { InjectAwsService } from '@backend/awsModule';
+import { LoggerService } from '@backend/logger';
 
 @Injectable()
 export class NotificationService {
-  constructor(@InjectAwsService(SNS) private sns: SNS) {}
+  constructor(
+    @InjectAwsService(SNS) private readonly sns: SNS,
+    private readonly logger: LoggerService,
+  ) {}
 
   async sendNotification(topicArn: string, message: string): Promise<void> {
+    this.logger.debug(`Sending notification to ${topicArn}`);
     const params = {
       Message: message,
       TopicArn: topicArn,
     };
     await this.sns.publish(params);
+    this.logger.debug(`Notification sent to ${topicArn}`);
   }
 }
