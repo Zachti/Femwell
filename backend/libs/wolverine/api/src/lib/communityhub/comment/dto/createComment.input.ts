79315dd5c6336a1151f6ffcf1b9124d9e0afa5f6
Zachti <114566720+Zachti@users.noreply.github.com>
2024-05-18 22:38:37 +0300

    backend - removed validations as graphql do it by itself.

--- backend/libs/wolverine/api/src/lib/communityhub/comment/dto/createComment.input.ts
+++ backend/libs/wolverine/api/src/lib/communityhub/comment/dto/createComment.input.ts
@@ -1,33 +1,24 @@
 import { InputType, Field } from '@nestjs/graphql';
 import { GraphQLString } from 'graphql/type';
-import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { GraphQLUUID } from 'graphql-scalars';
 
 @InputType()
 export class CreateCommentInput {
-  @IsNotEmpty()
-  @IsString()
   @Field(() => GraphQLString, {
     description: 'The username of the user',
   })
   username!: string;
 
-  @IsNotEmpty()
-  @IsString()
   @Field(() => GraphQLString, {
     description: 'The content of the comment',
   })
   content!: string;
 
-  @IsNotEmpty()
-  @IsUUID()
   @Field(() => GraphQLUUID, {
     description: 'The unique id of the user',
   })
   userId!: string;
 
-  @IsNotEmpty()
-  @IsUUID()
   @Field(() => GraphQLUUID, {
     description: 'The unique id of the post',
   })
--- backend/libs/wolverine/api/src/lib/liveChat/dto/createLiveChatDto.ts
+++ backend/libs/wolverine/api/src/lib/liveChat/dto/createLiveChatDto.ts
@@ -1,16 +1,11 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty, IsString, IsArray } from 'class-validator';
 import { GraphQLString } from 'graphql/type';
 
 @InputType()
 export class CreateLiveChatDto {
-  @Field()
-  @IsString()
-  @IsNotEmpty({ message: 'Name is required.' })
+  @Field(() => GraphQLString)
   name!: string;
 
-  @IsArray()
-  @IsNotEmpty({ message: 'Users Ids required.' })
   @Field(() => [GraphQLString])
   userIds!: string[];
 }
