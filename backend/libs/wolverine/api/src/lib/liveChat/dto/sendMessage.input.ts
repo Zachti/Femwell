462d3d0637f50b21246fe355c2d8b73ddba27eac
Zachti <114566720+Zachti@users.noreply.github.com>
2024-06-12 20:26:12 +0300

    backend - live chat updates.

--- backend/libs/wolverine/api/src/lib/liveChat/dto/sendMessage.input.ts
+++ backend/libs/wolverine/api/src/lib/liveChat/dto/sendMessage.input.ts
@@ -1,13 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
 import { GraphQLString } from 'graphql/type';
-import { GraphQLUUID } from 'graphql-scalars';
+import {GraphQLPositiveInt, GraphQLUUID} from 'graphql-scalars';
 
 @InputType()
 export class SendMessageInput {
   @Field(() => GraphQLUUID)
   userId!: string;
 
-  @Field(() => GraphQLString, { nullable: true })
+  @Field(() => GraphQLPositiveInt, { nullable: true })
   liveChatId!: number;
 
   @Field(() => GraphQLString)
